9c1f7973f6b0543f39ab870f8b69b03695196862
maazamin <maazamin8000@gmail.com>
2025-07-24 02:24:39 +0500

    utils discussion

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ dotenv.config({ path: './.env' }); // or just dotenv.config() if using .env
 
 const connectDB = require('./db/index');
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port: ${process.env.PORT}`);
+
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed: ", err);
+
+    })
 
 
 /*
