0a98ed5d2afcdfba0546f39553e641d2092edcd7
maazamin <maazamin8000@gmail.com>
2025-07-25 01:33:28 +0500

    add user and vedio model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,9 @@
 
-const asyncHandler = (fn) => {
-    (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-    }
-}
+const asyncHandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+};
 
-export { asyncHandler }
-
+export { asyncHandler };
 
 
 
