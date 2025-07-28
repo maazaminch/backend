44b08dd37f2d8ce6e1469e4fabb0ad5db27b1de0
maazamin <maazamin8000@gmail.com>
2025-07-29 00:36:11 +0500

    add new routers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 const { Router } = require("express");
-const { registerUser, loggedInUser, loggedOutUser, refreshAccessToken, changePassword, getCurrentUser } = require("../controllers/user.controller.js");
+const {
+    registerUser,
+    loggedInUser,
+    loggedOutUser,
+    refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory } = require("../controllers/user.controller.js");
+
 const upload = require('../middlewares/multer.middleware.js');
 const { verifyJWT } = require("../middlewares/auth.middleware.js");
 const router = Router()
@@ -23,6 +35,17 @@ router.route("/change-password").post(changePassword)
 //secure routes
 router.route("/logout").post(verifyJWT, loggedOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changePassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+//here we fetching data from params
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/watch-history").get(getWatchHistory)
+
 
 module.exports = router;
 
